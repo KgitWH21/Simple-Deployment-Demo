77c114d3d7d97a2477cc94cd4409ee5f171fdacd
kevin beier <kabeier@gmail.com>
2026-08-10 17:08:23 -0400

    starter app for phase IV deployment

--- server/task_api/settings.py
+++ server/task_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!jxkbdorz5qq54f8fr=xc8$u+%n*!614q!ym7!#$g)bm)@zm^x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'backend']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
